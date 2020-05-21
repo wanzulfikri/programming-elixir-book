5dd6fd984148a602125fbdc86f855f2cf493dcc4
wanzulfikri <wanzulfikri@gmail.com>
2020-05-22 05:45:38 +0800

    Complete exercise StringsAndBinaries-6 pg.131

---
+++ Ch11_StringsAndBinaries/binary.exs
@@ -0,0 +1,15 @@
+defmodule MyBinary do
+  def capitalize_sentences(sentences, acc \\ <<>>)
+
+  def capitalize_sentences(<<?., ?\s, tail::binary>>, acc) do
+    String.capitalize(acc) <> ". " <> capitalize_sentences(tail)
+  end
+
+  def capitalize_sentences(<<head::utf8, tail::binary>>, acc) do
+    capitalize_sentences(tail, acc <> <<head>>)
+  end
+
+  def capitalize_sentences(<<>>, _) do
+    <<>>
+  end
+end
