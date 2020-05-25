a38a6285a9e8a92b9a12a1131da1294fc43759ad
wanzulfikri <wanzulfikri@gmail.com>
2020-05-25 18:42:08 +0800

    Complete exercise StringsAndBinaries-7 pg.131 (how the heck was I able to complete this...a lot of luck involved)

--- Ch11_StringsAndBinaries/sales.exs
+++ Ch11_StringsAndBinaries/sales.exs
@@ -10,8 +10,26 @@ defmodule Sales do
 
     values =
       IO.stream(file, :line)
-      |> Enum.map(fn x -> String.split(x, [",", "\n"]) |> Enum.take(3) end)
+      |> Enum.map(fn x -> String.split(x, [",", "\n", " "]) |> Enum.take(3) end)
+      |> Enum.map(fn [first, second, third] ->
+        <<?:, tail::binary>> = second
+        IO.puts(tail)
+        [String.to_integer(first), String.to_atom(tail), String.to_float(third)]
+      end)
 
-    Enum.map(values, fn x -> Enum.zip(keys, x) |> Keyword.new() end)
+    Enum.map(values, fn x -> Enum.zip(keys, x) |> Keyword.new() end) |> sales_tax
+  end
+
+  @tax_rates [NC: 0.075, TX: 0.08]
+  def sales_tax(orders, tax_rates \\ @tax_rates) when is_list(orders) and is_list(tax_rates) do
+    for [id: _id, ship_to: ship_to, net_amount: net_amount] = order <- orders do
+      case Keyword.has_key?(tax_rates, ship_to) do
+        true ->
+          order ++ [total_amount: net_amount * (1 + Keyword.get(tax_rates, ship_to))]
+
+        _ ->
+          order ++ [total_amount: net_amount]
+      end
+    end
   end
 end
